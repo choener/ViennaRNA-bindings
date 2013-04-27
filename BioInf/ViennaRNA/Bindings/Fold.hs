674cc6db36b5e5c7cc8eb95cc8554461622f6a09
Christian Hoener zu Siederdissen <choener@tbi.univie.ac.at>
2013-04-28 01:23:20 +0200

    removed old file

--- BioInf/ViennaRNA/Bindings/Fold.hs
+++
@@ -1,8 +0,0 @@
-
-module BioInf.ViennaRNA.Bindings.Fold where
-
-import BioInf.ViennaRNA.Bindings.FFI.Fold as FFI
-import BioInf.ViennaRNA.Bindings.FFI.PartFunc as FFI
-
-fold = ffiFold
-eos i s = ffiEnergyOfStructure i s 0
