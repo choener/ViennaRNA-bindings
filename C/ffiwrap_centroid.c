44145f1018dc6697cc80d808b556f57aedd305c2
Christian Hoener zu Siederdissen <choener@bioinf.uni-leipzig.de>
2017-02-19 22:09:57 +0100

    disallow lonely basepairs
    currently some property tests are failing
    we keep it that way until parametrization is introduced

--- C/ffiwrap_centroid.c
+++ C/ffiwrap_centroid.c
@@ -23,7 +23,7 @@ double ffiwrap_centroid_temp (double temp, const char *sequence, char *structure
   vrna_md_t             md;
 
   vrna_md_set_default(&md);
-  md.noLP = 1
+  md.noLP = 1;
   md.temperature = temp;
   vc  = vrna_fold_compound(sequence, &md, 0);
   e = vrna_pf(vc, pf_struc);
