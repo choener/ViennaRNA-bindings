c34540864c71b69ecca3cd22601a0c15ac258e61
Christian Hoener zu Siederdissen <choener@bioinf.uni-leipzig.de>
2018-06-22 00:19:12 +0200

    improved default.nix

--- default.nix
+++ default.nix
@@ -1,27 +1,14 @@
-with import <nixpkgs> {};
+with (import <nixpkgs> {});
 with haskell.lib;
 
 rec {
-  hsPkgs = haskellPackages.extend (packageSourceOverrides {
-    BiobaseTypes = ../Lib-BiobaseTypes;
-    DPutils = ../Lib-DPutils;
-    ForestStructures = ../Lib-ForestStructures;
-    OrderedBits = ../Lib-OrderedBits;
-    PrimitiveArray = ../Lib-PrimitiveArray;
-    SciBaseTypes = ../Lib-SciBaseTypes;
-    ViennaRNA-bindings = ./.;
-  });
+  hsSrcSet = {ViennaRNA-bindings = ./.;};
+  hsPkgs = haskellPackages.extend (packageSourceOverrides hsSrcSet);
   hsShell = with hsPkgs; shellFor {
     packages = p: [ p.ViennaRNA-bindings ];
     withHoogle = true;
     buildInputs = [
-      cabal-install ghc alex happy c2hs syb language-c
-      BiobaseTypes
-      DPutils
-      ForestStructures
-      OrderedBits
-      PrimitiveArray
-      SciBaseTypes
+      cabal-install ghc
     ];
   };
 }
