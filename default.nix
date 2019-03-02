52c31ca494582a3027edb3c4bf89884ead936ac8
Christian Hoener zu Siederdissen <choener@bioinf.uni-leipzig.de>
2019-03-02 23:41:56 +0100

    move to envhs

--- default.nix
+++ default.nix
@@ -1,2 +1,20 @@
-with (import <nixpkgs> {});
-hsDevFunctions ./.
+{ mkDerivation, alex, array, base, BiobaseTypes, bytestring, c2hs
+, data-default-class, deepseq, happy, parallel, QuickCheck, stdenv
+, tasty, tasty-hunit, tasty-quickcheck, tasty-silver, tasty-th
+}:
+mkDerivation {
+  pname = "ViennaRNA-bindings";
+  version = "0.233.2.1";
+  src = ./.;
+  libraryHaskellDepends = [
+    array base bytestring data-default-class
+  ];
+  libraryToolDepends = [ alex c2hs happy ];
+  testHaskellDepends = [
+    array base BiobaseTypes bytestring deepseq parallel QuickCheck
+    tasty tasty-hunit tasty-quickcheck tasty-silver tasty-th
+  ];
+  homepage = "https://github.com/choener/ViennaRNA-bindings";
+  description = "ViennaRNA v2 bindings";
+  license = "unknown";
+}
