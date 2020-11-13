79679e857011b2c0f9ed45461cfe175f6d7068aa
Christian Hoener zu Siederdissen <choener@bioinf.uni-leipzig.de>
2020-11-13 21:19:06 +0100

    cleaned up functionality

--- BioInf/ViennaRNA/Bindings/Inline.hs
+++ BioInf/ViennaRNA/Bindings/Inline.hs
@@ -1,5 +1,7 @@
-{-# Language QuasiQuotes #-}
+{-# Language QuasiQuotes     #-}
 {-# Language TemplateHaskell #-}
+{-# Language BangPatterns    #-}
+{-# Language ViewPatterns    #-}
 
 module BioInf.ViennaRNA.Bindings.Inline where
 
@@ -7,11 +9,17 @@ import Data.ByteString.Char8 as BS
 import qualified Language.C.Inline as C
 import Foreign.C.Types
 import Foreign.Ptr
+import System.IO.Unsafe (unsafePerformIO)
+import qualified Data.ByteString.Internal as BI
+import Foreign.ForeignPtr.Unsafe
+
 
 
 C.context (C.baseCtx <> C.bsCtx)
 C.include "../C/ViennaRNA/hairpin_loops.h"
 C.include "../C/ViennaRNA/fold.h"
+
+-- | Create a default fold compound.
 
 mkFoldCompound :: BS.ByteString -> IO (Ptr ())
 mkFoldCompound inp = do
@@ -25,6 +33,8 @@ mkFoldCompound inp = do
   } |]
   return c
 
+-- | Destroy a given fold compound.
+
 destroyFoldCompound :: Ptr () -> IO ()
 destroyFoldCompound cptr = do
   [C.block| void {
@@ -32,29 +42,50 @@ destroyFoldCompound cptr = do
     vrna_fold_compound_free (c);
   } |]
 
-mfe :: BS.ByteString -> IO C.CFloat
-mfe inp = do
+-- | Allow operating with a fold compound, hiding the creation.
+
+withFoldCompound :: BS.ByteString -> (Ptr () -> a) -> a
+withFoldCompound inp f = unsafePerformIO $ do
   c <- mkFoldCompound inp
+  let !x = f c
+  destroyFoldCompound c
+  return x
+
+-- | Calculate the energy and structure of the given input sequence.
+
+mfe :: BS.ByteString -> (Double, BS.ByteString)
+mfe inp = unsafePerformIO $ do
+  c <- mkFoldCompound inp
+  let !out = BS.copy inp
   e <- [C.block| float {
     vrna_fold_compound_t * c = $(void *c);
-    vrna_mfe (c, 0);
+    vrna_mfe (c, $bs-ptr:out);
   } |]
   destroyFoldCompound c
-  return e
+  return (realToFrac e, out)
 
-hairpin :: BS.ByteString -> IO C.CInt
+-- | Calculate the energy of the sequence, assuming that it forms a hairpin.
+
+hairpin :: BS.ByteString -> Int
 hairpin inp = hairpinP inp 0 (fromIntegral $ BS.length inp -1)
 
-hairpinP :: BS.ByteString -> C.CInt -> C.CInt -> IO C.CInt
-hairpinP inp i j = do
+-- | Given some sequence, calculate the hairpin energy for the left and right position.
+
+hairpinP :: BS.ByteString -> Int -> Int -> Int
+hairpinP inp i j = unsafePerformIO $ do
   c <- mkFoldCompound inp
-  e <- hairpinCP c i j
+  let !e = hairpinCP c i j
   destroyFoldCompound c
   return e
 
-hairpinCP :: Ptr () -> C.CInt -> C.CInt -> IO C.CInt
-hairpinCP c i j = [C.block| int {
+-- | Low-level function that assumes a fold compound and returns the hairpin energy between the two
+-- indices.
+
+hairpinCP :: Ptr () -> Int -> Int -> Int
+hairpinCP c (fromIntegral -> i) (fromIntegral -> j) = unsafePerformIO $ do
+  e <- [C.block| int {
     vrna_fold_compound_t * c = $(void *c);
     vrna_eval_hp_loop (c, $(int i) , $(int j));
   } |]
+  return $ fromIntegral e
 
