672d759d37aa3933dc9069987856a27f1c1f5952
Christian Hoener zu Siederdissen <christian.hoener.zu.siederdissen@uni-jena.de>
2022-06-30 19:11:31 +0200

    minor fixups

--- default.nix
+++ default.nix
@@ -1,4 +1,4 @@
-{ mkDerivation, array, base, bytestring, data-default-class
+{ mkDerivation, array, base, bytestring, c2hs, data-default-class
 , inline-c, lib
 }:
 mkDerivation {
@@ -8,6 +8,7 @@ mkDerivation {
   libraryHaskellDepends = [
     array base bytestring data-default-class inline-c
   ];
+  libraryToolDepends = [ c2hs ];
   homepage = "https://github.com/choener/ViennaRNA-bindings";
   description = "ViennaRNA v2 bindings";
   license = "unknown";
